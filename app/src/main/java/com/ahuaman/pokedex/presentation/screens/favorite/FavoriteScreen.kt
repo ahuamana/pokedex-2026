9fc82fe8a732bc7c45fd2552bfadbe5c8fef8430
Antony Nicolas Huaman Alikhan”git config --global user.email “nicolasalikhan@gmail.com” <73962071@continental.edu.pe>
2026-02-07 08:36:02 -0500

    added: favorite pokemon screen

--- app/src/main/java/com/ahuaman/pokedex/presentation/screens/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/ahuaman/pokedex/presentation/screens/favorite/FavoriteScreen.kt
@@ -1,20 +1,49 @@
 package com.ahuaman.pokedex.presentation.screens.favorite
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ahuaman.domain.model.PokemonPresentationModel
+import com.ahuaman.pokedex.presentation.screens.home.components.PokemonItem
+import com.ahuaman.testing.pokemons.PokemonMocks
 
 @Composable
-fun FavoriteScreen(modifier: Modifier = Modifier) {
-    Column() {
-        Text("Favorite Screen")
+fun FavoriteScreen(
+    modifier: Modifier = Modifier,
+    favoritePokemonList: List<PokemonPresentationModel> = emptyList()
+    ) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(bottom = 16.dp, top = 16.dp, start = 16.dp, end = 16.dp),
+            verticalArrangement = androidx.compose.foundation.layout.Arrangement.spacedBy(16.dp),
+            horizontalArrangement = androidx.compose.foundation.layout.Arrangement.spacedBy(16.dp)
+        ) {
+            items(favoritePokemonList.count()) { index ->
+                PokemonItem(
+                    pokemon = favoritePokemonList[index],
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 private fun FavoriteScreenPrev() {
-    FavoriteScreen()
+    FavoriteScreen(
+        modifier = Modifier.fillMaxSize(),
+        favoritePokemonList = PokemonMocks.fullPokedexList
+    )
 }
